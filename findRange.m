73761054b67c5bf23de413fd72360f712e36db81
Abhishek Vijayan <abhivij.game@gmail.com>
2014-11-09 22:48:25 +0530

    added ignore

---
+++ findRange.m
@@ -0,0 +1,7 @@
+function findRange(y, y_test)
+	fprintf('Training set\n');
+	fprintf('Min = %d  Max = %d\n', min(y), max(y)); 
+
+	fprintf('Test set\n');
+	fprintf('Min = %d  Max = %d\n', min(y_test), max(y_test));
+end
